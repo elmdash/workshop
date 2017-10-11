840c4a3fda875a0927330024aeef9f6e63cf78fa
Peter Sooley <psoots@gmail.com>
2017-10-10 19:42:21 -0700

    Re-organizing again

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,5 +3,5 @@
 sudo apt update
 sudo apt upgrade
 sudo apt install git ansible aptitude
-mkdir -p ~/projects/elmdash
-git clone https://psoots@bitbucket.org/elmdash/workshop.git projects/elmdash/workshop
+mkdir -p ~/Projects/ElmDash
+git clone https://psoots@bitbucket.org/elmdash/workshop.git Projects/ElmDash/workshop
