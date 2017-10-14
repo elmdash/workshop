3f89747faca5cb1281d935d1ef003b40470b634e
Peter Sooley <psoots@gmail.com>
2017-10-13 20:15:07 -0700

    moving things around again

--- make/tasks/install.mk
+++ make/tasks/install.mk
@@ -1,10 +1,20 @@
-TASKS += install
+TASKS += install install-system install-secrets install-apps
 USAGE += .install-usage
 
 
+install: install-system install-secrets install-apps
 
-install:
-	ansible-playbook install.yml --ask-become-pass --ask-vault-pass
+install-system:
+	ansible-playbook -i hosts/home/local books/system.yml --ask-become-pass
+
+install-secrets:
+	ansible-playbook -i hosts/home-secure/local books/secrets.yml --ask-become-pass --ask-vault-pass
+
+install-apps:
+	ansible-playbook -i hosts/home/local books/apps.yml --ask-become-pass
 
 .install-usage:
-	$(call describe,"install","install configured software for this system")
+	$(call describe,"install","install everything")
+	$(call describe,"install-system","install system requirements")
+	$(call describe,"install-secrets","install ssh configurations or other secrets")
+	$(call describe,"install-apps","install applications")
