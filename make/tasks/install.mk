d01ef795bbfa454b5c381ce7607b94120d130ac5
Peter Sooley <psoots@gmail.com>
2017-10-14 10:29:45 -0700

    re-organizing again. simpler

--- make/tasks/install.mk
+++ make/tasks/install.mk
@@ -1,20 +1,8 @@
-TASKS += install install-system install-secrets install-apps
+TASKS += install
 USAGE += .install-usage
 
-
-install: install-system install-secrets install-apps
-
-install-system:
-	ansible-playbook -i hosts/home/local books/system.yml --ask-become-pass
-
-install-secrets:
-	ansible-playbook -i hosts/home-secure/local books/secrets.yml --ask-become-pass --ask-vault-pass
-
-install-apps:
-	ansible-playbook -i hosts/home/local books/apps.yml --ask-become-pass
+install:
+	ansible-playbook install.yml --ask-become-pass --ask-vault-pass
 
 .install-usage:
 	$(call describe,"install","install everything")
-	$(call describe,"install-system","install system requirements")
-	$(call describe,"install-secrets","install ssh configurations or other secrets")
-	$(call describe,"install-apps","install applications")
