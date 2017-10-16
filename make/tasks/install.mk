fcfd3a109715e5f84f9bfe3ed639c38366501076
Peter Sooley <psoots@gmail.com>
2017-10-15 18:46:39 -0700

    adding some terminal settings w/dconf

--- make/tasks/install.mk
+++ make/tasks/install.mk
@@ -4,5 +4,8 @@ USAGE += .install-usage
 install:
 	ansible-playbook install.yml --ask-become-pass --ask-vault-pass
 
+install-verbose:
+	ansible-playbook install.yml --ask-become-pass --ask-vault-pass -vvv
+
 .install-usage:
 	$(call describe,"install","install everything")
