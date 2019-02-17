0bc80eb37c342febee20b57d108ddaef177a9406
Peter Sooley <psoots@gmail.com>
2019-02-16 16:02:45 -0800

    [F] Installing make

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,6 @@
 sudo add-apt-repository ppa:ansible/ansible
 sudo apt update
 sudo apt upgrade
-sudo apt install git ansible aptitude
+sudo apt install git ansible aptitude build-essential
 mkdir -p ~/Projects/ElmDash
 git clone https://github.com/elmdash/workshop.git ~/Projects/ElmDash/workshop
